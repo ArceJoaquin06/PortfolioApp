b6f34a611785a080974f6beb712ca19fa62018a2
Joaquin Arce <d47174994@alumnos.itr.edu.ar>
2024-11-18 23:09:47 -0300

    no me anda el css

--- static/js/app.js
+++ static/js/app.js
@@ -1,61 +1,57 @@
-var nextBtn = document.querySelector('.next'),
+// Seleccionar los elementos necesarios
+const nextBtn = document.querySelector('.next'),
     prevBtn = document.querySelector('.prev'),
     carousel = document.querySelector('.carousel'),
-    list = document.querySelector('.list'), 
-    item = document.querySelectorAll('.item'),
-    runningTime = document.querySelector('.carousel .timeRunning') 
+    list = document.querySelector('.list');
 
-let timeRunning = 3000 
-let timeAutoNext = 7000
+// Definir los tiempos para las transiciones
+const timeRunning = 3000; // Tiempo para completar una animación
+const timeAutoNext = 7000; // Tiempo entre cada cambio automático
 
-nextBtn.onclick = function(){
-    showSlider('next')
-}
+// Agregar eventos a los botones de navegación
+nextBtn.onclick = function () {
+    console.log("Next button clicked");
+    showSlider('next');
+};
 
-prevBtn.onclick = function(){
-    showSlider('prev')
-}
+prevBtn.onclick = function () {
+    console.log("Prev button clicked");
+    showSlider('prev');
+};
 
-let runTimeOut 
+// Variable para manejar el tiempo de animación manual
+let runTimeOut;
 
+// Configurar el cambio automático de diapositivas
 let runNextAuto = setTimeout(() => {
-    nextBtn.click()
-}, timeAutoNext)
+    nextBtn.click();
+}, timeAutoNext);
 
-
-function resetTimeAnimation() {
-    runningTime.style.animation = 'none'
-    runningTime.offsetHeight /* trigger reflow */
-    runningTime.style.animation = null 
-    runningTime.style.animation = 'runningTime 7s linear 1 forwards'
-}
-
-
+// Función para mostrar el slider en la dirección especificada
 function showSlider(type) {
-    let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
-    if(type === 'next'){
-        list.appendChild(sliderItemsDom[0])
-        carousel.classList.add('next')
-    } else{
-        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
-        carousel.classList.add('prev')
+    // Obtener los elementos actuales del carrusel
+    const sliderItemsDom = list.querySelectorAll('.carousel .list .item');
+    
+    if (type === 'next') {
+        // Mover el primer elemento al final
+        list.appendChild(sliderItemsDom[0]);
+        carousel.classList.add('next');
+    } else {
+        // Mover el último elemento al principio
+        list.prepend(sliderItemsDom[sliderItemsDom.length - 1]);
+        carousel.classList.add('prev');
     }
 
-    clearTimeout(runTimeOut)
+    // Eliminar las clases después de la animación
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carousel.classList.remove('next');
+        carousel.classList.remove('prev');
+    }, timeRunning);
 
-    runTimeOut = setTimeout( () => {
-        carousel.classList.remove('next')
-        carousel.classList.remove('prev')
-    }, timeRunning)
-
-
-    clearTimeout(runNextAuto)
+    // Reiniciar el temporizador automático
+    clearTimeout(runNextAuto);
     runNextAuto = setTimeout(() => {
-        nextBtn.click()
-    }, timeAutoNext)
-
-    resetTimeAnimation() // Reset the running time animation
+        nextBtn.click();
+    }, timeAutoNext);
 }
-
-// Start the initial animation 
-resetTimeAnimation()
